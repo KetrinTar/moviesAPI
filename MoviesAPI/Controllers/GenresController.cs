2924344794b6528eebb1968c0ecdf22ee2f1630f
KetrinTar <fire.ice.ketrin@gmail.com>
2022-08-31 18:54:03 +0300

    Initial Commit

---
+++ MoviesAPI/Controllers/GenresController.cs
@@ -0,0 +1,70 @@
+﻿using AutoMapper;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.ModelBinding;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Logging;
+using MoviesAPI.DTOs;
+using MoviesAPI.Entities;
+using MoviesAPI.Filters;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MoviesAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class GenresController : ControllerBase
+    {       
+        private readonly ILogger<GenresController> logger;
+        private readonly ApplicationDbContext context;
+        private readonly IMapper mapper;
+
+        public GenresController(ILogger<GenresController> logger, 
+            ApplicationDbContext context,
+            IMapper mapper)
+        {
+            this.logger = logger;
+            this.context = context;
+            this.mapper = mapper;
+        }
+
+        [HttpGet]
+        public async Task<ActionResult<List<GenreDTO>>> Get()
+        {
+            logger.LogInformation("Getting all the genres");
+            var genres= await context.Genres.ToListAsync();
+            return mapper.Map<List<GenreDTO>>(genres);
+            
+        }
+        [HttpGet("{id:int}/", Name = "getGenre")]
+        public ActionResult<Genre> Get(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        [HttpPost]
+        public async Task<ActionResult> Post([FromBody] GenreCreationDTO genreCreationDTO)
+        {
+            var genre = mapper.Map<Genre>(genreCreationDTO);
+            context.Genres.Add(genre);
+            await context.SaveChangesAsync();
+            return NoContent();
+        }
+
+        [HttpPut]
+        public ActionResult Put([FromBody] Genre genre)
+        {
+            throw new NotImplementedException();
+        }
+
+        [HttpDelete]
+        public ActionResult Delete()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
